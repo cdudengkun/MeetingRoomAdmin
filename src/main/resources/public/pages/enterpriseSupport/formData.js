c25148061c538ee6af4dd75e85a88815d0f74d8b
947960731@qq.com <947960731@qq.com>
2021-11-16 00:15:51 +0800

    企业助力页面

---
+++ src/main/resources/public/pages/enterpriseSupport/formData.js
@@ -0,0 +1,101 @@
+layui.use(['form','layer', 'baseConfig', "upload"], function () {
+    var form = layui.form,
+        $ = layui.jquery,
+        layer = parent.layer === undefined ? layui.layer : top.layer,
+        upload = layui.upload,
+        baseConfig = layui.baseConfig;
+
+    var pageName = "enterpriseSupport";
+    var data = baseConfig.getDataFromList( pageName);
+    var actionType = baseConfig.getUrlParamer( "actionType");
+
+
+    /**
+     * 将list页面通过url传过来的参数加载到form表单里面去
+     * @param data
+     */
+    if( data){
+        baseConfig.loadFormData( data);
+    }
+
+    /**
+     * 处理表单元素的 禁用、隐藏、显示灯操作
+     */
+    function handleFormItem(){
+        //只有查看详细或者审核的时候，才显示审核区域
+        if( actionType != baseConfig.ACTION.REVIEW && actionType != baseConfig.ACTION.DETAIL){
+            $( "#review_div").hide();
+        }
+        //审核的时候，只有内容区域是只读
+        if( actionType == baseConfig.ACTION.REVIEW){
+            baseConfig.setFormReadOnly( "content_div", true);
+        }
+        //查看详细的时候，内容区域和审核区域都只读
+        if( actionType == baseConfig.ACTION.DETAIL){
+            baseConfig.setFormReadOnly( "content_div", true);
+            baseConfig.setFormReadOnly( "review_div", true);
+            $( "#submitDiv").hide();
+        }else{
+            $( "#closeDiv").hide();
+        }
+
+    }
+    handleFormItem();
+
+    //关闭弹窗页面
+    $( "#closeBtn").on( "click", function(){
+        var index = parent.layer.getFrameIndex( window.name);
+        parent.layer.close( index);
+    });
+    //上传封面
+    upload.render({
+        elem: '#uploadFile',
+        url: '/file/upload?type=enterpriseSupportFile',
+        multiple: true,
+        before: function(obj){ //obj参数包含的信息，跟 choose回调完全一致，可参见上文。
+            obj.preview(function(index, file, result) {
+                var size = file.size;//文件大小填写到数据表里面，这里的单位是byte
+                if( parseInt( size / 1024) > 0){//转化为KB
+                    if( parseInt(size / 1024 / 1024) > 0){//转化为MB
+                        size = parseInt(size / 1024 /1024) + "MB";
+                    }else{
+                        size = parseInt(size / 1024) + "KB";
+                    }
+                }else{
+                    size = size + "B";
+                }
+
+                $( "input[name=size]").val( size);
+            });
+        },
+        done: function(res){
+            if( res.code == 200){
+                var filePath = res.data.filePath;
+                $( "input[name=url]").val( filePath);
+            }
+        }
+    });
+
+
+    //监听提交
+    form.on('submit(saveBtn)', function (data) {
+        $.ajaxSettings.async = false;
+        //弹出loading
+        var index = top.layer.msg('数据提交中，请稍候',{icon: 16,time:false,shade:0.8});
+        var jsonBody = data.field;
+        //提交数据
+        $.post("/" + pageName + "/addOrUpdate", jsonBody, function( res){
+            if( res.code == 200){
+                top.layer.close( index);
+                top.layer.msg( res.msg);
+                layer.closeAll( "iframe");
+                //刷新父页面
+                parent.location.reload();
+            }else{
+                top.layer.close( index);
+                top.layer.msg( res.msg);
+            }
+        });
+    });
+
+});
---
+++ src/main/resources/public/pages/enterpriseSupport/list.js
@@ -0,0 +1,106 @@
+layui.use(['form', 'table', 'util', 'baseConfig'], function () {
+    var $ = layui.jquery,
+        form = layui.form,
+        baseConfig = layui.baseConfig,
+        util = layui.util;
+        table = layui.table;
+
+    //------------公共配置
+    var pageName = "enterpriseSupport";
+    var minWidth = 900;
+    var minHeight = 500;
+    var formTitleSuffix = "企业助力";
+
+    table.render({
+        id : "listTable",
+        elem: '#currentTableId',
+        url : '/' + pageName + '/list',
+        toolbar: '#toolbar',
+        defaultToolbar: [],
+        cols: [[
+            {field: 'name', width: 300, title: '名称'},
+            {field: 'url', width: 500, title: '下载地址'},
+            {field: 'size', width: 150, title: '文件大小'},
+            {field: 'downloadCount', width: 150, title: '下载次数'},
+            {field: 'createTime', width: 200, title: '创建时间', templet : function( d){
+                return util.toDateString( d.createTime);
+            }},
+            {title: '操作', minWidth: 150, toolbar: '#currentTableBar', align: "center"}
+        ]],
+        limits: [10, 15, 20, 25, 50, 100],
+        limit: 15,
+        page: true,
+        response : {
+            statusCode: 200 //规定成功的状态码，默认：0
+        },
+        parseData : function( res){ //res 即为原始返回的数据
+            return {
+                "code": res.code, //解析接口状态
+                "msg": res.msg, //解析提示文本
+                "count": res.data.count, //解析数据长度
+                "data": res.data.data //解析数据列表
+            };
+        }
+    });
+
+    //------------表单搜索
+    form.on('submit(data-search-btn)', function (data) {
+        var result = data.field;
+        //执行搜索重载
+        table.reload('listTable', {
+            page: {
+                curr: 1
+            }
+            , where: result
+        }, 'data');
+
+        return false;
+    });
+
+    //------------表格头部工具栏，一般放新增等按钮
+    table.on('toolbar(currentTableFilter)', function (obj) {
+        if (obj.event === 'add') {  // 监听添加操作
+            baseConfig.sendDataToForm( pageName, null);
+            var index = layer.open({
+                title: baseConfig.getTitleByType( 1, formTitleSuffix),
+                type: 2,
+                shade: 0.2,
+                shadeClose: true,
+                area: [ baseConfig.getWidth( minWidth), baseConfig.getHeight( minHeight)],
+                content: 'formData.html',
+            });
+        }
+    });
+
+    //------------表格数据工具栏，一般放表格行数据的编辑、删除等按钮
+    table.on('tool(currentTableFilter)', function (obj) {
+        var data = obj.data;
+        if (obj.event === 'detail') {
+            baseConfig.sendDataToForm( pageName, data);
+            var index = layer.open({
+                title: baseConfig.getTitleByType( 3, formTitleSuffix),
+                type: 2,
+                shade: 0.2,
+                shadeClose: true,
+                area: [ baseConfig.getWidth( minWidth), baseConfig.getHeight( minHeight)],
+                content: 'formData.html?actionType=3',
+            });
+        } else if (obj.event === 'edit') {
+            baseConfig.sendDataToForm( pageName, data);
+            var index = layer.open({
+                title: baseConfig.getTitleByType( 2, formTitleSuffix),
+                type: 2,
+                shade: 0.2,
+                shadeClose: true,
+                area: [ baseConfig.getWidth( minWidth), baseConfig.getHeight( minHeight)],
+                content: 'formData.html?actionType=2',
+            });
+        } else if (obj.event === 'delete') {
+            layer.confirm('确认删除？', function (index) {
+                obj.del();
+                layer.close(index);
+            });
+        }
+    });
+
+});
